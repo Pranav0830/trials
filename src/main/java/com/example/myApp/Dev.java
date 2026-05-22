a1cef8f7f217e81f4cb97eb5f0da72436088c2ca
SWPRANA <SWPRANA@msc.corpdir.net>
2026-05-22 11:35:05 +0530

    initial commit

---
+++ src/main/java/com/example/myApp/Dev.java
@@ -0,0 +1,18 @@
+package com.example.myApp;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+    @Autowired //field injection
+    private Laptop laptop;
+
+
+    public void build(){
+
+        laptop.compile();
+        System.out.println("Working on Awesome project");
+    }
+}
---
+++ src/main/java/com/example/myApp/Laptop.java
@@ -0,0 +1,12 @@
+package com.example.myApp;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+    public void compile(){
+        System.out.println("Compiling the myApp files");
+    }
+}
